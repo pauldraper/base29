c7a2577156b53bee5d69d4376cd4eb1f2d38050a
Paul Draper <paulddraper@gmail.com>
2019-12-30 15:58:51 -0700

    Implement

---
+++ base29.py
@@ -0,0 +1,28 @@
+import string
+
+BASE = 29
+CHARS = "".join(c for c in string.digits + string.ascii_lowercase if c not in "01ailou")
+
+
+def encode_number(number):
+    result = ""
+    while number:
+        result += CHARS[number % BASE]
+        number //= BASE
+    return result[::-1]
+
+
+def decode_number(chars):
+    value = 0
+    for c in reversed(chars):
+        value *= BASE
+        value += index(CHARS, c)
+    return value
+
+
+if __name__ == "__main__":
+    import random
+
+    for _ in range(10):
+        number = random.randint(0, 2 ** 64 - 1)
+        print("{:020d}\t{:0>14}".format(number, encode_number(number)))
